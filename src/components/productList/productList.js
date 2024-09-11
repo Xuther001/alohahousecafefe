f62a189aff280f93af00d847db07b9b70aeb2cf5
osmac <macos@osmacs-MBP.lan>
2024-09-11 16:23:36 +0700

    deleted quantity field and submit button, not needed

--- src/components/productList/productList.js
+++ src/components/productList/productList.js
@@ -4,18 +4,12 @@ import './productList.css';
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
-  const [quantities, setQuantities] = useState({});
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
         const products = await getAllProducts();
         setProducts(products);
-        const initialQuantities = products.reduce((acc, product) => {
-          acc[product.id] = 0;
-          return acc;
-        }, {});
-        setQuantities(initialQuantities);
       } catch (error) {
         console.error('Failed to fetch products:', error);
       }
@@ -23,18 +17,6 @@ const ProductList = () => {
 
     fetchProducts();
   }, []);
-
-  const handleQuantityChange = (productId, event) => {
-    const value = Math.max(0, event.target.value);
-    setQuantities((prevQuantities) => ({
-      ...prevQuantities,
-      [productId]: value,
-    }));
-  };
-
-  const handleSubmit = (productId) => {
-    console.log(`Submitting quantity ${quantities[productId]} for product ID ${productId}`);
-  };
 
   const formatCurrency = (amount) => {
     return new Intl.NumberFormat('vi-VN', {
@@ -45,7 +27,6 @@ const ProductList = () => {
 
   return (
     <div className="product-list-container">
-      <h2></h2>
       {products.length === 0 ? (
         <p>No products available.</p>
       ) : (
@@ -61,22 +42,6 @@ const ProductList = () => {
                 <strong>{product.name}</strong>
                 <p>{product.description}</p>
                 <p className="price">Price: {formatCurrency(product.price)}</p>
-                <div className="quantity-container">
-                  <label htmlFor={`quantity-${product.id}`}>Quantity:</label>
-                  <input
-                    id={`quantity-${product.id}`}
-                    type="number"
-                    min="0"
-                    value={quantities[product.id] || 0}
-                    onChange={(event) => handleQuantityChange(product.id, event)}
-                  />
-                </div>
-                <button
-                  className="submit-button"
-                  onClick={() => handleSubmit(product.id)}
-                >
-                  Add to Cart
-                </button>
               </div>
             </li>
           ))}
